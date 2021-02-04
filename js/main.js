0ed8c78c4e607c4994b7045ee4ae3301c8a8c6e3
Ivan Lysukhin <ilysukhin@mail.ru>
2021-02-04 11:09:28 +0300

    Добавил вспомогательных функций

--- js/main.js
+++ js/main.js
@@ -7,48 +7,80 @@ let generateNumber = function (min, max, point) {
   return Number(coordinate.toFixed(point));  //https://learn.javascript.ru/number#okruglenie
 }
 
+const MIN_COORDINATE_X = 35.65000;
+const MAX_COORDINATE_X = 35.70000;
+const MIN_COORDINATE_Y = 139.70000;
+const MAX_COORDINATE_Y = 139.80000;
+const NUMBERS_AFTER_POINT = 5;
+const NUMBER_PROFILES = 10;
+
+let photosLinks = [1, 2, 3, 4, 5, 6, 7, 8];
 let houseTypes = ['palace', 'flat', 'house', 'bungalow'];
 let times = ['12:00', '13:00', '14:00'];
 let featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 let photosArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
+let getRandomElementNoRepeat = function (array) {
+  let randomElementIndex = generateNumber(0, array.length - 1);
+  let randomElement = array[randomElementIndex];
+  array.splice(randomElementIndex, 1);
+  return randomElement;
+}
+
+let getRandomElement = function (array) {
+  let randomElementIndex = generateNumber(0, array.length - 1);
+  return array[randomElementIndex];
+}
+
+let randomPositiveNumber = () => {
+  return generateNumber(0 , Number.MAX_SAFE_INTEGER)
+}
+
+let getNoRepeatList = (array) => {
+  return array.slice(0, generateNumber(0,array.length))
+}
+
 let createProfile = () => {
   return {
     author: {
-      avatar: 'img/avatars/user' + '0' + generateNumber(1, 8) + '.png', //строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
+      avatar: 'img/avatars/user' + '0' + getRandomElementNoRepeat(photosLinks) + '.png', //строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
     },
 
     offer: {
       title: 'Объявление об аренде',
 
-      address: location.x + ', ' + generateNumber(139.70000, 139.80000, 5), //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
+      address: generateNumber(MIN_COORDINATE_X, MAX_COORDINATE_X, NUMBERS_AFTER_POINT) + ', ' + generateNumber(MIN_COORDINATE_Y, MAX_COORDINATE_Y, NUMBERS_AFTER_POINT), //строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
 
-      price: generateNumber(0, 1000000), //число — стоимость. Любое положительное число.
+      price: randomPositiveNumber(), //число — стоимость. Любое положительное число.
 
-      type: houseTypes[generateNumber(0, houseTypes.length -1)], //строка — одно из четырёх фиксированных значений: palace, flat, house или bungalow.
+      type: getRandomElement(houseTypes), //строка — одно из четырёх фиксированных значений: palace, flat, house или bungalow.
 
-      rooms: generateNumber(0, 10), //число — количество комнат. Любое положительное число.
+      rooms: randomPositiveNumber(), //число — количество комнат. Любое положительное число.
 
-      guests:  generateNumber(0, 20), //число — количество гостей, которое можно разместить. Любое положительное число.
+      guests:  randomPositiveNumber(), //число — количество гостей, которое можно разместить. Любое положительное число.
 
-      checkin: times[generateNumber(0, times.length -1)], //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+      checkin: getRandomElement(times), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
-      checkout: times[generateNumber(0, times.length -1)], //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+      checkout: getRandomElement(times), //строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
-      features: new Array(generateNumber(0, featuresArr.length)).fill(null).map(() => { return featuresArr[generateNumber(0, featuresArr.length -1)]}), //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+      features:  getNoRepeatList(featuresArr), //массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
 
       description: 'Лучшее место для отдыха и работы', //строка — описание помещения. Придумайте самостоятельно.
 
-      photos: new Array(generateNumber(0, photosArr.length)).fill(null).map(() => {return photosArr[generateNumber(0, photosArr.length -1)]}), //массив строк — массив случайной длины из значений: http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg.
+      photos: getNoRepeatList(photosArr), //массив строк — массив случайной длины из значений: http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg.
     },
 
     location: {
-      x: generateNumber(35.65000, 35.70000, 5), //число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000
-      y: generateNumber(139.70000, 139.80000, 5), //число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
+      x: generateNumber(MIN_COORDINATE_X, MAX_COORDINATE_X, NUMBERS_AFTER_POINT), //число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000
+      y: generateNumber(MIN_COORDINATE_Y, MAX_COORDINATE_Y, NUMBERS_AFTER_POINT), //число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
     },
   }
 }
 
-let profiles = new Array(10).fill(null).map(() => createProfile());
+// let profiles = new Array(10).fill(null).map(() => createProfile());
 
-profiles.forEach();
+let profiles = [];
+
+for (let i = 0; i <= NUMBER_PROFILES - 1; i++) {
+  profiles[i] = createProfile();
+}
